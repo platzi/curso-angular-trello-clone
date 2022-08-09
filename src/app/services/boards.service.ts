3f0d32d7030fe6282d9b1d1427db6fe6d4f1cdd9
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-09 16:21:29 -0400

    :construction: calc position

--- src/app/services/boards.service.ts
+++ src/app/services/boards.service.ts
@@ -12,6 +12,7 @@ import { Card } from '@models/card.model';
 })
 export class BoardsService {
   apiUrl = environment.API_URL;
+  bufferSpace = 65535;
 
   constructor(private http: HttpClient) {}
 
@@ -23,17 +24,21 @@ export class BoardsService {
 
   getPosition(cards: Card[], currentIndex: number) {
     if (cards.length === 1) {
-      return 'is new';
+      return this.bufferSpace;
     }
     if (cards.length > 1 && currentIndex === 0) {
-      return 'is the top';
+      const onTopPosition = cards[1].position;
+      return onTopPosition / 2;
     }
     const lastIndex = cards.length - 1;
     if (cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex) {
-      return 'is the middle';
+      const prevPosition = cards[currentIndex - 1].position;
+      const nextPosition = cards[currentIndex + 1].position;
+      return (prevPosition + nextPosition) / 2;
     }
     if (cards.length > 1 && currentIndex === lastIndex) {
-      return 'is the bottom';
+      const onBottomPosition = cards[lastIndex - 1].position;
+      return onBottomPosition + this.bufferSpace;
     }
     return 0;
   }
