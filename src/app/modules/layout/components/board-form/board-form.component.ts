9536f96b13b34aed4f8bfa3268f5a6f7ffe5ed5e
Nicolas Molina Monroy <hi@nicobytes.com>
2022-08-09 18:11:01 -0400

    :construction: close overlay

--- src/app/modules/layout/components/board-form/board-form.component.ts
+++ src/app/modules/layout/components/board-form/board-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
@@ -10,6 +10,8 @@ import { Colors } from '@models/colors.model';
   templateUrl: './board-form.component.html'
 })
 export class BoardFormComponent {
+
+  @Output() closeOverlay = new EventEmitter<boolean>();
 
   form = this.formBuilder.nonNullable.group({
     title: ['', [Validators.required]],
@@ -30,6 +32,7 @@ export class BoardFormComponent {
       const { title, backgroundColor } = this.form.getRawValue();
       this.boardService.createBoard(title, backgroundColor)
       .subscribe(board => {
+        this.closeOverlay.next(false);
         this.router.navigate(['/app/boards', board.id]);
       })
     } else {
--- src/app/modules/layout/components/navbar/navbar.component.ts
+++ src/app/modules/layout/components/navbar/navbar.component.ts
@@ -35,4 +35,8 @@ export class NavbarComponent {
     this.router.navigate(['/login']);
   }
 
+  close(event: boolean) {
+    this.isOpenOverlayCreateBoard = event;
+  }
+
 }
